ab955c53906da8b7e4901b9ca9686f128cd937e5
RFHold <robert@robhold.com>
2019-06-29 02:21:03 -0400

    Basic running server with typescript dependencies

---
+++ app/app.ts
@@ -0,0 +1,11 @@
+// lib/app.ts
+import express from "express"
+
+const PORT = process.env.PORT || 3000
+
+// Create a new express application instance
+const app: express.Application = express();
+
+app.listen(PORT, function () {
+    console.log(`App listening on port ${PORT}`);
+});
